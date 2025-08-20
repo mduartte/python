fd6bad4ef75b0fe8f33632ef70c6fb114bcdbb13
Mateus Miranda <mmiranda@R11Travel.local>
2025-08-20 14:13:38 -0300

    introduzindo

---
+++ musicas/musica.py
@@ -0,0 +1,21 @@
+#pedindo nome do user em py
+
+nome = input("Digite seu nome: ")
+
+#pedindo idade do user em py
+# int = numero inteiro
+
+idade = int(input("Digite sua idade: "))
+
+ano_500 = 2025 - idade + 500
+
+
+num1 = int(input("Digite o primeiro numero: "))
+
+num1 = int(input("Digite o primeiro numero: "))
+
+soma = num1 + num2
+
+print(f"Bom, {nome}! O resoltado é: ", soma)
+
+print(f"Olá {nome}! Você vai completar 500 anos no ano {ano_500}.")
